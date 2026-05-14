3591c477bddd2452c1d99aba42823d006b865f2d
Dexming <rafaelavos.com.br@gmail.com>
2026-05-14 09:22:25 +0300

    a simple fork that fixes some bugs and adds thumbnail optionality

--- hello.py
+++ hello.py
@@ -1,13 +1,13 @@
 import os
 import bsky_api
 
-path = './images/'
-handle = "plaster.gay"
+path = '../Thingys/Saved Pictures/etc/shift+barra/'
+handle = "cold-foxx56.bsky.social"
 
 posts = bsky_api.get_liked_posts(handle)
 images = bsky_api.get_image_urls(posts)
 
 #print("should be a massive list of stuff: ",images)
 
-os.makedirs(path, exist_ok = True) #is this okay????
-bsky_api.save_images(images,path)
+os.makedirs(path, exist_ok = True) #is this okay???? #probably lol
+bsky_api.save_images(images, path, thumbnails = False)
